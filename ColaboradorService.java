3e037468cd698ede2ca0f947cf0b1c076e42600c
Rudrian <162451660+Rudrian@users.noreply.github.com>
2025-03-11 11:56:49 -0300

    Update ColaboradorService.java

--- ColaboradorService.java
+++ ColaboradorService.java
@@ -1,5 +1,36 @@
 package com.projetojpa.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetojpa.Entity.Colaborador;
+import com.projetojpa.Repository.ColaboradorRepository;
+
+@Service
 public class ColaboradorService {
 
+    private final ColaboradorRepository colaboradorRepository;
+
+    @Autowired
+    public ColaboradorService(ColaboradorRepository colaboradorRepository) {
+        this.colaboradorRepository = colaboradorRepository;
+    }
+
+    public Colaborador saveColaborador(Colaborador colaborador) {
+        return colaboradorRepository.save(colaborador);
+    }
+
+    public Colaborador getColaboradorById(Long id) {
+        return colaboradorRepository.findById(id).orElse(null);
+    }
+
+    public List<Colaborador> getAllColaboradores(){
+        return colaboradorRepository.findAll();
+    } 
+
+    public void deleteColaborador(Long id) {
+        colaboradorRepository.deleteById(id);
+    }
 }
